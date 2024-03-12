fc11790d2002256312158a74a102dcbc6309ae3b
gsanto1973 <93636042+gsanto1973@users.noreply.github.com>
2024-03-12 17:28:14 -0500

    Terminación desarrollo Backend 3

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  
+
   //await app.listen(process.env.PORT || 3000);
   await app.listen(process.env.PORT ?? 3000);
 }
